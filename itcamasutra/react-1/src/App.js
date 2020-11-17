9ad4bc09cb3bd752d265af29eada36b20481b67a
Philip <philip_bmx11@yahoo.com>
2020-11-17 23:07:56 +0300

    itcamasutra react-1 11 lesson;

--- itcamasutra/react-1/src/App.js
+++ itcamasutra/react-1/src/App.js
@@ -1,20 +1,40 @@
+import React from 'react';
 import './App.css';
-import Header from "./components/Header/Header";
-import Technologies from "./components/Technologies/Technologies";
 import Button from "./components/Button/Button";
 
 const App = () => {
     return (
-        <div>
-            <p>before header</p>
-            <Header/>
-            <p>after header</p>
-            <Technologies/>
-
-            <div>
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img src="logo-finished.png" alt="logo"/>
+            </header>
+            <nav className='nav'>
+                <div><a href="#">Profile</a></div>
+                <div><a href="#">Messages</a></div>
+                <div><a href="#">Music</a></div>
+                <div><a href="#">Settings</a></div>
+                <br/>
                 <Button kind="primary1" onClick={() => console.log("Кнопка нажата!")}>
                     Привет, мир!
                 </Button>
+            </nav>
+            <div className='content'>
+                <div>
+                    <img src="https://www.publicdomainpictures.net/pictures/320000/nahled/background-image.png" alt="image"/>
+                </div>
+                <div>
+                    Ava + description
+                </div>
+                <div>
+                    my posts
+                    <div>
+                        New posts
+                    </div>
+                    <div>
+                        <div>post1</div>
+                        <div>post2</div>
+                    </div>
+                </div>
             </div>
         </div>
     );
